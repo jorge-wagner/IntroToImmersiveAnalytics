0f2b5b40263703ad24217812ac0ac1e9efa84d35
Jorge Wagner <jawfilho@inf.ufrgs.br>
2023-01-20 15:14:33 -0300

    New example with IATK-based scatterplot.

--- Assets/Scripts/ScatterplotPlotterWithIATK.cs
+++ Assets/Scripts/ScatterplotPlotterWithIATK.cs
@@ -17,17 +17,6 @@ public class ScatterplotPlotterWithIATK : MonoBehaviour
 
     }
 
-
-    // A reusable method to create an IATK CSVDataSource object.
-    CSVDataSource createCSVDataSource(string data)
-    {
-        CSVDataSource dataSource;
-        dataSource = gameObject.AddComponent<CSVDataSource>();
-        dataSource.load(data, null);
-        return dataSource;
-    }
-
-    
     public void LoadPoints(string datasetPath)
     {
         TextAsset myDataSource = Resources.Load(datasetPath) as TextAsset;
@@ -40,21 +29,48 @@ public class ScatterplotPlotterWithIATK : MonoBehaviour
                              setDataDimension(myCSVDataSource["x"].Data, ViewBuilder.VIEW_DIMENSION.X).
                              setDataDimension(myCSVDataSource["y"].Data, ViewBuilder.VIEW_DIMENSION.Y).
                              setDataDimension(myCSVDataSource["z"].Data, ViewBuilder.VIEW_DIMENSION.Z).
-                             setSingleColor(Color.white);
-                             
-                             //setSize(myCSVDataSource["Base"].Data).
-                             //setColors(myCSVDataSource["Time"].Data.Select(x => g.Evaluate(x)).ToArray());
+                             setColors(getListOfColorsFromListOfHexStrings(myCSVDataSource, "alternativeColorMap"));                    
 
         // Use the "IATKUtil" class to get the corresponding Material mt 
-        Material mt = IATKUtil.GetMaterialFromTopology(AbstractVisualisation.GeometryType.Points);
+        Material mt = IATKUtil.GetMaterialFromTopology(AbstractVisualisation.GeometryType.Spheres);
         mt.SetFloat("_MinSize", 0.025f);
         mt.SetFloat("_MaxSize", 0.025f);
 
         // Create a view builder with the point topology
         View view = vb.updateView().apply(gameObject, mt);
 
-
+        view.transform.position -= new Vector3(0.5f, 0.5f, 0.5f); // compensating for the fact that natively the center of the view object is the lower left corner of the visualization and not its centroid.
     }
 
+
+
+    // A reusable method to create an IATK CSVDataSource object.
+    CSVDataSource createCSVDataSource(string data)
+    {
+        CSVDataSource dataSource;
+        dataSource = gameObject.AddComponent<CSVDataSource>();
+        dataSource.load(data, null);
+        return dataSource;
+    }
+
+
+    // Improvised function to retrieve data from a column comprising color strings in hex format 
+    Color[] getListOfColorsFromListOfHexStrings(CSVDataSource csvds, string colorColumnIdentifier)
+    {
+        float[] iatk_float_data = csvds[colorColumnIdentifier].Data;
+        Color[] newColorList = new Color[iatk_float_data.Length];
+        int i = 0;
+
+        foreach (float f in iatk_float_data)
+        {
+            Color newCol;
+            if (ColorUtility.TryParseHtmlString(csvds.getOriginalValue(f, colorColumnIdentifier).ToString(), out newCol))
+            {
+                newColorList[i++] = newCol;
+            }
+        }
+
+        return newColorList;
+    }
 
 }
